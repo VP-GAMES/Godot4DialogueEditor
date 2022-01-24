dbcb8d88e6b1a4f86b1ca75427e0e9753c54d0b7
Vladimir Petrenko <vladipetrenko@googlemail.com>
2022-01-24 01:50:44 +0100

    V 0.0.1(1.4.0) First running version

---
+++ dialogue/DialogueDialogues.gd
@@ -0,0 +1,10 @@
+# Names for DialogueManger to use in source code: MIT License
+# @author Vladimir Petrenko
+@tool
+class_name DialogueDialogues
+
+const DIALOGUE = "9669fde1-d104-4bd7-a5f9-1644eaeb5fcc"
+
+const DIALOGUES = [
+ "Dialogue"
+]
